e753b9d4eb9ec64be12633976de8070b8b6a15d2
James <jamestse1114@gmail.com>
2023-07-12 08:09:16 +0100

    modified the webscrapping file to use dash and plotly instead of flask, as a web application; modifited the calculation.py allowing user inputting raster file and classification bins

--- calculation.py
+++ calculation.py
@@ -1,5 +1,6 @@
 import rasterio
 import matplotlib.pyplot as plt
+import numpy as np
 
 class SiteSuitability:
     def __init__(self, raster_path):
@@ -14,17 +15,25 @@ class SiteSuitability:
         # Replace this with your actual calculation
         self.suitability = raster_values / raster_values.max()
 
+    def classify_suitability(self, bins):
+        # Classify the suitability scores into bins
+        self.suitability = np.digitize(self.suitability, bins)
+
     def plot_suitability(self):
         # Create a plot of the suitability
         plt.imshow(self.suitability, cmap='hot', interpolation='nearest')
         plt.colorbar(label='Suitability')
         plt.show()
 
-# Execution
+
 site_suitability = SiteSuitability('path_to_your_raster_data')
 
 # Calculate suitability
 site_suitability.calculate_suitability()
+
+# Classify suitability into bins
+bins = [0, 0.2, 0.4, 0.6, 0.8, 1]  # replace with your actual bin edges
+site_suitability.classify_suitability(bins)
 
 # Plot suitability
 site_suitability.plot_suitability()
--- webscrapping.py
+++ webscrapping.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, jsonify
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import plotly.graph_objects as go
 import requests
 import json
-
-app = Flask(__name__)
 
 class DataRetriever:
     def __init__(self, api_url):
@@ -10,44 +11,46 @@ class DataRetriever:
 
     def get_data(self, endpoint, params=None):
         response = requests.get(self.api_url + endpoint, params=params)
-        
-        # Print out the status code and response text
-        print('Status code:', response.status_code)
-        print('Response text:', response.text)
-        
-        # Check if the request was successful
+
         if response.status_code == 200:
-            # Parse the response as JSON
             data = json.loads(response.text)
             return data
-        
         else:
             print(f'Request failed with status code {response.status_code}')
             return None
 
-
-# Initialize the DataRetriever with the URL of the public API
 data_retriever = DataRetriever('https://globalsolaratlas.info/api')
 
-@app.route('/api/data', methods=['GET'])
-def get_data_from_api():
-    # Get the endpoint and parameters from the request
-    endpoint = request.args.get('endpoint')
-    params = request.args.get('params')
+app = dash.Dash(__name__)
 
-    # Check if endpoint was provided
+app.layout = html.Div([
+    dcc.Input(id='endpoint-input', type='text', placeholder='Enter endpoint'),
+    dcc.Input(id='params-input', type='text', placeholder='Enter parameters as JSON'),
+    html.Button('Submit', id='submit-button'),
+    dcc.Graph(id='data-graph')
+])
+
+@app.callback(
+    Output('data-graph', 'figure'),
+    [Input('submit-button', 'n_clicks')],
+    [dash.dependencies.State('endpoint-input', 'value'),
+     dash.dependencies.State('params-input', 'value')]
+)
+def update_graph(n_clicks, endpoint, params):
     if endpoint is None:
-        return jsonify({'error': 'No endpoint provided'}), 400
+        return go.Figure()
 
-    # If params is a string of JSON, parse it into a dictionary
     if params is not None and isinstance(params, str):
         params = json.loads(params)
 
-    # Use the DataRetriever to get the data from the public API
     data = data_retriever.get_data(endpoint, params)
 
-    # Return the data to the user
-    return jsonify(data)
+    if data is None:
+        return go.Figure()
+
+    fig = go.Figure(data=go.Bar(y=list(data.values()), x=list(data.keys())))
+
+    return fig
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run_server(debug=True)

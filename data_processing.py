30b9ac7e8fea4f3c7e5bee2a32fc933a417cab3f
James <jamestse1114@gmail.com>
2023-08-01 22:42:15 +0100

    updated the data processing code and the data of HK case study

---
+++ data_processing.py
@@ -0,0 +1,189 @@
+import os
+import ast
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import geopandas as gpd
+import rasterio
+import fiona
+import contextily as ctx
+import matplotlib.font_manager as fm
+from fiona.crs import from_epsg
+from matplotlib.patches import FancyArrow
+from scipy.ndimage import distance_transform_edt
+from scipy.ndimage import distance_transform_edt
+from rasterio.features import rasterize
+from rasterio.mask import mask
+from rasterio.transform import from_bounds
+from rasterio.warp import calculate_default_transform, reproject, Resampling
+from rasterio.features import rasterize
+from shapely.geometry import mapping, Polygon
+from shapely.geometry import Polygon
+from shapely.ops import unary_union
+from mpl_toolkits.axes_grid1.anchored_artists import AnchoredSizeBar
+
+
+def get_directory_name():
+    
+    # Ask the user for the directory name
+    directory_name = input("Please enter the directory name: ")
+
+    # Check if the directory exists
+    if not os.path.exists(directory_name):
+        print(f"The directory {directory_name} does not exist.")
+        return None
+
+    return directory_name
+
+directory_name = "hk_wind_turbine_site_selection_case_study"
+
+
+def study_area_removed_contraints(directory_name, study_area_file_name, constraint_layers_folder_name):
+    
+    study_area_path = os.path.join(directory_name, 'data', study_area_file_name)
+    constraint_layers_path = os.path.join(directory_name, constraint_layers_folder_name)
+    
+    # Load the study area shapefile
+    study_area = gpd.read_file(study_area_path)
+
+    # Start a list to contain the constraints
+    constraints = []
+
+    # Loop through the constraint shapefiles
+    for constraint_file in os.listdir(constraint_layers_path):
+        
+        # Only load the shapefiles
+        if constraint_file.endswith('.shp'):
+            
+            # Load the constraint shapefile
+            constraint = gpd.read_file(os.path.join(constraint_layers_path, constraint_file))
+            
+            # Convert the crs of the constraint layers to the crs of the study area
+            if constraint.crs != study_area.crs:
+                constraint = constraint.to_crs(study_area.crs)
+            
+            # Add the constraints to the list
+            constraints.append(constraint)
+
+    # Concatenate all GeoDataFrames in the list
+    constraints = pd.concat(constraints)
+
+    # Perform the spatial operation to deduct the constraint area from the study area
+    processed_study_area = gpd.overlay(study_area, constraints, how='difference')
+
+    # Plot the study area which does not include constraint area
+    processed_study_area.plot()
+    plt.show()
+
+    # Save the processed study area to a new shapefile
+    output_path = os.path.join(directory_name, 'study_area', 'study_area_without_constraints.shp')
+    processed_study_area.to_file(output_path)
+
+# study_area_removed_contraints(get_directory_name(), 'hk_boundary.shp', 'constraint_layers')
+
+
+def reclassify_raster_layer(directory_name, input_file_name, output_folder_name, reclass_dictionary):
+    
+    input_file_path = os.path.join(directory_name, 'data', input_file_name)
+    output_folder_path = os.path.join(directory_name, "criteria_layers")
+    
+    # Check if the input file exists
+    if not os.path.exists(input_file_path):
+        print(f"The file {input_file_path} does not exist.")
+        return
+
+    # Load the raster layer
+    with rasterio.open(input_file_path) as layer:
+        raster_data = layer.read(1)
+        meta = layer.meta
+
+    # Print the min and max values of the raster data
+    print(f"Min value: {np.min(raster_data)}")
+    print(f"Max value: {np.max(raster_data)}")
+
+    # Print the class definitions
+    print("Class definitions:")
+    for class_num, class_value in reclass_dictionary.items():
+        print(f"Class {class_num}: {class_value}")
+
+    # Create a new array for the reclassified data
+    reclassified = np.zeros(raster_data.shape, dtype=np.uint8)
+
+    # Apply the reclassification
+    for new_value, (low, high) in reclass_dictionary.items():
+        reclassified[(raster_data >= low) & (raster_data < high)] = new_value
+
+    # Create a new folder if it doesn't exist
+    if not os.path.exists(output_folder_path):
+        os.makedirs(output_folder_path)
+
+    # Write the reclassified data to a new raster file
+    output_file_path = os.path.join(output_folder_path, f'reclassified_{os.path.basename(input_file_path)}')
+    with rasterio.open(output_file_path, 'w', **meta) as dest:
+        dest.write(reclassified, 1)
+
+# reclassification_dict = {1: (150, float('inf')),
+#     2: (120, 150),
+#     3: (80, 120),
+#     4: (40, 80),
+#     5: (float('-inf'), 40)}
+# reclassify_raster_layer(directory_name, 'elevation.tif', 'criteria_layers', reclassification_dict)
+
+
+def plot_raster(directory_name, file_name):
+    file_path = os.path.join(directory_name, file_name)
+
+    # Open the raster file
+    with rasterio.open(file_path) as src:
+        # Read the raster data
+        raster_data = src.read(1)
+
+    # Create a new figure
+    plt.figure(figsize=(10,10))
+
+    # Display the raster data
+    plt.imshow(raster_data, cmap='viridis')
+
+    # Add a colorbar
+    plt.colorbar(label='Raster Value')
+
+    # Display the plot
+    plt.show()
+
+# plot_raster(directory_name, 'criteria_layers/reclassified_elevation.tif')
+
+
+def plot_shapefile(directory_name, file_name, base_map_file_name=None):
+    
+    file_path = os.path.join(directory_name, file_name)
+
+    # Load the shapefile
+    gdf = gpd.read_file(file_path)
+
+    # Create a new figure
+    fig, ax = plt.subplots(figsize=(10,10))
+
+    # If a base map file name is provided, load and plot it first
+    if base_map_file_name:
+        base_map_path = os.path.join(directory_name, base_map_file_name)
+        base_map = gpd.read_file(base_map_path)
+        
+        # Convert the CRS of the shapefile to match the base map
+        gdf = gdf.to_crs(base_map.crs)
+        
+        base_map.plot(ax=ax, color='white', edgecolor='black')
+
+    # Plot the shapefile
+    gdf.plot(ax=ax, color='red')
+
+    # Grid
+    ax.grid(True)
+
+    # North arrow
+    arrow = FancyArrow(0.9, 0.85, dx=0, dy=0.05, width=0.01, color='k', transform=fig.transFigure)
+    fig.patches.append(arrow)
+    plt.text(0.9, 0.9, 'N', transform=fig.transFigure)
+
+    plt.show()
+
+plot_shapefile(directory_name, 'study_area/study_area_without_constraints.shp', 'data/hk_boundary.shp')
